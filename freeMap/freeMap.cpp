30c7774d1aba81c4ded0803f6a9ab3134974292c
Megumi-X <tangkeke0109@gmail.com>
2023-11-22 23:54:53 +0800

    temp

--- freeMap/freeMap.cpp
+++
@@ -1,7 +0,0 @@
-#include "freeMap.h"
-
-void freeMap()
-{
-	for(int i=0;i<N+2;++i) delete[] Map[i];
-	delete[] Map;
-}
--- over/over.h
+++
@@ -1,8 +0,0 @@
-#ifndef Over_H
-#define Over_H
-#include<bits/stdc++.h>
-extern int **Map; 
-extern int N; 
-extern int movenum; 
-bool over(); 
-#endif
